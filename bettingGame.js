dab4a5206d0fc90ffe3b5e97e30d578967e0fd63
awarewolfyukon <simon.pregent@hec.ca>
2015-06-02 07:44:23 +0000

    restart button doesn't quite work yet.

--- bettingGame.js
+++ bettingGame.js
@@ -1,70 +1,132 @@
+$('document').ready(function() {
 
-var startingBalance = 100;
-var betRange = [5,10];
-var guessRange = [1,10];
-
-var getBetInRange = function(rangeBottom,rangeTop) {
-  var bet;
-  do {
-    bet = prompt("Enter a bet between $" + rangeBottom + " and $" + rangeTop);
-  } while (!(bet >= rangeBottom && bet <= rangeTop))
-  return bet;
-}
-
-var getGuessNumberInRange = function(rangeBottom,rangeTop) {
-  var guess;
-  do {
-    guess = prompt("Enter a number between " + rangeBottom + " and " + rangeTop);
-  } while (!(guess >= rangeBottom && guess <= rangeTop))
-  return guess;
-}
-
-var getRandNumberInRange = function(rangeBottom,rangeTop) {
-  number = Math.floor((Math.random() * rangeTop) + rangeBottom);
-  return number;
-}
-
-var showBankAccount = function(bankAccount) {
-  message = "Your bank account balance is now $" + bankAccount + ".";
-  return message;
-}
-
-var stillAlive = function(bankAccount, betRangeBottom) {
-  if (bankAccount >= betRangeBottom) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-var logGameVars = function(number,guess,bankAccount) {
-  console.log("NUMBER : " + number);
-  console.log("GUESS : " + guess);
-  console.log("BALANCE : " + bankAccount);
-}
-
-var play = function(startingBalance, betRange, guessRange) {
-
+  var startingBalance = 100;
   bankAccount = startingBalance;
-  do {
-    bet = getBetInRange(betRange[0],betRange[1]);
-    guess = getGuessNumberInRange(guessRange[0],guessRange[1]);
-    number = getRandNumberInRange(guessRange[0],guessRange[1]);
-    if (number == guess) {
-      bankAccount += 2 * bet;
-      logGameVars(number,guess,bankAccount);
-      alert("Bang on!" + " " + showBankAccount(bankAccount));
-    } else if (!(guess == number + 1 || guess == number - 1)) {
-      bankAccount -= bet;
-      logGameVars(number,guess,bankAccount);
-      alert("Sorry, try again!" + " " + showBankAccount(bankAccount));
+  var betRange = [5,10];
+  var guessRange = [1,10];
+
+  var showAlert = function (id,type,text) {
+
+    var title_type;
+
+    if (type == 'danger') {
+      var title_type = 'error';
     } else {
-      logGameVars(number,guess,bankAccount);
-      alert("Close but no cigar!" + " " + showBankAccount(bankAccount));
+      title_type = type;
     }
 
-  } while (stillAlive(bankAccount, betRange[0]))
-  alert("You ran out of money." + " " + showBankAccount(bankAccount) + " " + "Thanks for playing.");
-}
+    title = id.charAt(0).toUpperCase() + id.slice(1) + " " + title_type.charAt(0).toUpperCase() + title_type.slice(1)
 
-play(startingBalance, betRange, guessRange);
+    alert_html = "<div id=\"" + id + "-alert\" class=\"alert alert-" + type + " alert-dismissible fade in\" role=\"alert\"> \
+                <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">×</span></button> \
+                <h4>"+ title + "</h4> \
+                <p>" + text + "</p> \
+              </div>";
+
+    $(alert_html).hide().appendTo("#alert-pane").fadeIn(1500);
+  }
+
+  var getBetInRange = function(rangeBottom,rangeTop) {
+
+    var bet;
+
+    bet = $('#bet').val();
+
+    if (!(bet >= rangeBottom && bet <= rangeTop)) {
+      alert_text = "The bet value is not between $" + rangeBottom + " and $" + rangeTop;
+
+      showAlert('bet', 'danger', alert_text);
+
+      return false;
+    }
+    return bet;
+  }
+
+  var getGuessNumberInRange = function(rangeBottom,rangeTop) {
+
+    var guess;
+
+    guess = $('#guess').val();
+
+    if (!(guess >= rangeBottom && guess <= rangeTop)) {
+      alert_text = "Your guess is not between " + rangeBottom + " and " + rangeTop;
+
+      showAlert('guess', 'danger', alert_text);
+
+      return false;
+    }
+    return guess;
+  }
+
+  var getRandNumberInRange = function(rangeBottom,rangeTop) {
+    number = Math.floor((Math.random() * rangeTop) + rangeBottom);
+    return number;
+  }
+
+  var updateBankAccount = function(bankAccount) {
+    $("#balance-body").text(bankAccount)
+  }
+
+  var stillAlive = function(betRangeBottom) {
+    console.log("STILL ALIVE? = "+ (bankAccount >= betRangeBottom))
+    return (bankAccount >= betRangeBottom)
+  }
+
+  var logGameVars = function(number,guess,bankAccount) {
+    console.log("NUMBER : " + number);
+    console.log("GUESS : " + guess);
+    console.log("BALANCE : " + bankAccount);
+  }
+
+  var play = function(betRange, guessRange) {
+
+    $(".alert").fadeOut('fast');
+
+    bet = getBetInRange(betRange[0],betRange[1]);
+    guess = getGuessNumberInRange(guessRange[0],guessRange[1]);
+
+    number = getRandNumberInRange(guessRange[0],guessRange[1]);
+    $("#number").text(number);
+
+    if (bet && guess) {
+      if (number == guess) {
+        bankAccount += 2 * bet;
+
+        logGameVars(number,guess,bankAccount);
+        $("#balance-body").text(bankAccount);
+        showAlert('guess', 'success', 'Bang on!');
+
+      } else if (!(guess == number + 1 || guess == number - 1)) {
+        bankAccount -= bet;
+
+        logGameVars(number,guess,bankAccount);
+        $("#balance-body").text(bankAccount);
+        showAlert('guess', 'info', 'Sorry, try again!');
+
+      } else {
+        logGameVars(number,guess,bankAccount);
+        $("#balance-body").text(bankAccount);
+        showAlert('guess', 'warning', 'Close but no cigar!');
+      }
+
+      if (!(stillAlive(betRange[0]))) {
+        $("#balance-body").text(bankAccount);
+        showAlert('game', 'info', 'You ran out of money. Thanks for playing.');
+        $("#play").fadeOut('fast');
+      }
+    }
+  }
+
+  $("#balance-body").text(startingBalance);
+
+  bet_placeholder_text = "Enter a bet between $" + betRange[0] + " and $" + betRange[1];
+  $("#bet").attr("placeholder", bet_placeholder_text);
+
+  guess_placeholder_text = "Enter a number between " + guessRange[0] + " and " + guessRange[1];
+  $("#guess").attr("placeholder", guess_placeholder_text);
+
+  $('#play').on('click', function() {
+    play(betRange, guessRange);
+  });
+
+});
